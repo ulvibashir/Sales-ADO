8b4f9e3439d0679dbc9ec8c21b427614543ca40b
Ulvi Bashir <ubashirov@outlook.com>
2020-01-30 23:18:21 +0400

    Listbox Changed to DataGrid

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -34,17 +34,17 @@ namespace SalesDepart
             using (SqlSalesRepository repo = new SqlSalesRepository(connectionString, factory))
             {
 
-                //repo.CreateDB();
-                //repo.CreateTables();
-                //repo.InsertCustomer(1, "Ulvi", "Bashirov");
-                //repo.InsertCustomer(2, "Elvin", "Ismayilov");
-                //repo.InsertCustomer(3, "Johhny", "Depp");
-                //repo.InsertSeller(1, "Abraham", "Lincoln");
-                //repo.InsertSeller(2, "Bill", "Gates");
-                //repo.InsertSeller(3, "Steve", "Jobs");
-                //repo.InsertSales(1, 1, 1, 50.25, new DateTime(2020, 01, 20, 20, 15, 0));
-                //repo.InsertSales(2, 2, 2, 125.30, new DateTime(2020, 01, 21, 20, 12, 10));
-                //repo.InsertSales(3, 3, 3, 5.75, new DateTime(2020, 01, 22, 21, 07, 10));
+                repo.CreateDB();
+                repo.CreateTables();
+                repo.InsertCustomer(1, "Ulvi", "Bashirov");
+                repo.InsertCustomer(2, "Elvin", "Ismayilov");
+                repo.InsertCustomer(3, "Johhny", "Depp");
+                repo.InsertSeller(1, "Abraham", "Lincoln");
+                repo.InsertSeller(2, "Bill", "Gates");
+                repo.InsertSeller(3, "Steve", "Jobs");
+                repo.InsertSales(1, 1, 1, 50.25, new DateTime(2020, 01, 20, 20, 15, 0));
+                repo.InsertSales(2, 2, 2, 125.30, new DateTime(2020, 01, 21, 20, 12, 10));
+                repo.InsertSales(3, 3, 3, 5.75, new DateTime(2020, 01, 22, 21, 07, 10));
                 //repo.DropTables();
                 //repo.DropDB();
 
